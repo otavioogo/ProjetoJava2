61047cd5e0958eac97adaf5614babcc925c449ea
otavi <otavi@yak.mshome.net>
2022-04-05 13:15:48 -0300

    commit1

--- ProjetoJava2/src/Pessoa.java
+++ ProjetoJava2/src/Pessoa.java
@@ -1,4 +1,27 @@
 
 public class Pessoa {
-	//
+	
+	private String nome;
+	
+	private String Sobrenome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return Sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		Sobrenome = sobrenome;
+	}
+	////
+	
+	
 }
+
